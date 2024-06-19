2133c0859c71eb35e4cc0ad2084af666ee070e9b
Adrian Sampson <adrian@radbox.org>
2024-06-19 18:45:28 -0400

    Attempt to make unprefixed symlinks

---
+++ container/mklinks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+BIN=/opt/riscv/bin
+PREFIX=riscv32-unknown-linux-gnu
+
+# Look for all the executables in $BIN starting with $PREFIX and symlink them
+# from an unprefixed (plain) name.
+for fn in $BIN/$PREFIX-* ; do
+    plainname=${fn#$BIN/$PREFIX-}
+    ln -s $fn $BIN/$plainname
+done
