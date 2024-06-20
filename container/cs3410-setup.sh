3bda2fc709e66016d83fbe2db8afa1adaf4fe540
Adrian Sampson <adrian@radbox.org>
2024-06-20 17:36:48 -0400

    Remove unused script

--- container/cs3410-setup.sh
+++
@@ -1,6 +0,0 @@
-# SETUP SCRIPT FOR CS 3410
-
-export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
-alias gcc='riscv32-unknown-linux-gnu-gcc -Wall -Wshadow -Wvla -Werror -pedantic'
-alias gdb='riscv32-unknown-linux-gnu-gdb'
-alias qemu='qemu-riscv32 -L /opt/riscv/sysroot'
