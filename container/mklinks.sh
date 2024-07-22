620ff9099b9134b7301687a5f94a91c15d0c2383
Adrian Sampson <adrian@radbox.org>
2024-07-21 21:02:47 -0400

    Fix up mklinks for boring container

--- container/mklinks.sh
+++ container/mklinks.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 BIN=/opt/riscv/bin
-PREFIX=riscv64-unknown-linux-gnu
+PREFIX=riscv64-linux-gnu
+SUFFIX=12
 
 # Look for all the executables in $BIN starting with $PREFIX and symlink them
 # from an unprefixed (plain) name.
 for fn in $BIN/$PREFIX-* ; do
     plainname=${fn#$BIN/$PREFIX-}
+    plainname=${plainname%-$SUFFIX}
     ln -s $fn $BIN/$plainname
 done
