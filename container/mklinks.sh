19cd540a8caba5be66f172ea302d5f9018ab2bf7
Adrian Sampson <adrian@radbox.org>
2024-07-21 21:10:58 -0400

    Symlinks in separate location

--- container/mklinks.sh
+++ container/mklinks.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-BIN=/opt/riscv/bin
+BIN=/usr/bin
+DEST=/opt/riscv/bin
 PREFIX=riscv64-linux-gnu
 SUFFIX=12
 
 # Look for all the executables in $BIN starting with $PREFIX and symlink them
 # from an unprefixed (plain) name.
+mkdir -p $DEST
 for fn in $BIN/$PREFIX-* ; do
     plainname=${fn#$BIN/$PREFIX-}
     plainname=${plainname%-$SUFFIX}
-    ln -s $fn $BIN/$plainname
+    ln -s $fn $DEST/$plainname
 done
